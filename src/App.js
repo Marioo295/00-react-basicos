faea856e4c746add21c765628422315532038c95
Marioo295 <mramosgaliana@gmail.com>
2022-09-14 16:55:30 +0200

    Componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
+import Componente from './components/Componente';
 import './App.css';
 
 function App() {
-  let nombre = "Jon"
-  let auth = true;
-  let estaciones = ["Primavera","Verano","Otoño","Invierno"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <label htmlFor="nombre">Nombre</label>
-        <input type="text"id="nombre"/>
-        <h1>{nombre}</h1>
-        <p>{auth ? "El usuario está logueado":"El usuario no está logueado"}</p>
-        <p>{2+1}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <ul>
-          {estaciones.map((el,index)=>(<li key={index}>{el}</li>))}
-        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -29,6 +20,14 @@ function App() {
           Learn React
         </a>
       </header>
+      <section>
+        <Componente msg="Hola soy un componente funcional expresado desde una prop"/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      </section>
     </div>
   );
 }
