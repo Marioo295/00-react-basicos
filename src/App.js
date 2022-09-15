8c6975f31d42e10d97c9c8d91fee4b6134185859
Marioo295 <mramosgaliana@gmail.com>
2022-09-15 09:43:03 +0200

    Propiedades React

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import Componente from './components/Componente';
+import Propiedades from './components/Propiedades';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
+          <Componente msg="Hola soy un componente funcional expresado desde una prop"/>
+          <hr></hr>
+          <Propiedades 
+            cadena="Esto es una cadena de texto" 
+            numero={19}
+            booleano={false}
+            arreglo={[1,2,3]}
+            objeto={{nombre:"Mario",correo:"mario@gmail.com"}}
+            elementoReact={<i>Esto es un elemento React</i>}
+            funcion={num => num * num}
+            componenteReact={<Componente msg="Soy un componente pasado como prop"/>}
+          />
+        </section>
       </header>
-      <section>
-        <Componente msg="Hola soy un componente funcional expresado desde una prop"/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-      </section>
     </div>
   );
 }
-
 export default App;
